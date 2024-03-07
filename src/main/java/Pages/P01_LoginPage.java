208e35890cdaa136fb04c7f684c8eafcc75c5fdb
mahmoudelhamrawi <mahmoudelhamrawi5@gmail.com>
2024-03-07 18:08:01 +0200

    add valid login feature

--- src/main/java/Pages/P01_LoginPage.java
+++ src/main/java/Pages/P01_LoginPage.java
@@ -1,11 +1,41 @@
 package Pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class P01_LoginPage extends pageBase{
     public P01_LoginPage(WebDriver driver) {
         super(driver);
     }
+
+
+    private final By userNameInp  = By.cssSelector("input[name=\"username\"]");
+    private final By passwordInp = By.cssSelector("input[name=\"password\"]") ;
+    private final By btnSubmit = By.cssSelector("button[type=\"submit\"]") ;
+    private final By logoImgToAssert = By.cssSelector("div[class=\"oxd-brand-banner\"]") ;
+    private final By UlListAdmin = By.cssSelector("ul[class=\"oxd-main-menu\"]");
+    private final By liListAdmin = By.cssSelector("li[class=\"oxd-main-menu-item-wrapper\"]") ;
+
+    public void fillLoginForm(String name , String password)
+    {
+        enterTxt(driver.findElement(userNameInp),name );
+        enterTxt(driver.findElement(passwordInp),password );
+        clickELe(driver.findElement(btnSubmit));
+    }
+    public  WebElement logImgAssert()
+    {
+      return   driver.findElement(logoImgToAssert) ;
+    }
+    public List<WebElement> listOfAdmin()
+    {
+        return driver.findElements(liListAdmin);
+    }
+
+
+
 
 
 
--- src/test/java/TestCases/TC01_LoginTest.java
+++ src/test/java/TestCases/TC01_LoginTest.java
@@ -1,6 +1,26 @@
 package TestCases;
 
+import Pages.P01_LoginPage;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
 public class TC01_LoginTest extends testBase{
+
+    P01_LoginPage loginPage ;
+    WebDriverWait wait = new WebDriverWait(driver , Duration.ofSeconds(30));
+     @Test
+    public void validLogin()
+    {
+      loginPage = new P01_LoginPage(driver);
+      loginPage.fillLoginForm("Admin", "admin123");
+      wait.until(ExpectedConditions.visibilityOf(loginPage.logImgAssert())) ;
+      Assert.assertEquals("https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index" ,driver.getCurrentUrl());
+
+    }
 
 
 
